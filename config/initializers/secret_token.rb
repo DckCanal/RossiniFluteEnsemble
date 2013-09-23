877cc9a9728dddd7c13e9b95dd519e32171e9fa0
Marco De Canal <marco.decanal@gmail.com>
2013-09-23 15:03:28 +0200

    Commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RossiniFluteEnsemble::Application.config.secret_key_base = '59476b0054213589ce91953cd316091b6a082625cefec95db60ccfc3f1f73d3364f00b51b7bf0763a9b5342daee4c2ffb0e3e2f8b29b897fdd636156be720aad'
